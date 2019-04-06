2fa445a308cf0d957fd9771eded8dca51c0e2cee
unknown <dbrudne1@gmail.com>
2019-04-06 13:20:54 -0500

    adding ant design

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import App, { Container } from "next/app";
+import React from "react";
+import stylesheet from "antd/dist/antd.min.css";
+
+export default class MyApp extends App {
+	render() {
+		const { Component, pageProps } = this.props;
+		return (
+			<Container>
+				<Component {...pageProps} />
+			</Container>
+		);
+	}
+}
