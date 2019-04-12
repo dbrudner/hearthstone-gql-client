2fdf616367fb89fc90f821b78d4eba00fc11c36b
unknown <dbrudne1@gmail.com>
2019-04-11 21:28:57 -0500

    adding search bar and basic options

---
+++ features/search/basic-filter.tsx
@@ -0,0 +1,36 @@
+import { Select } from "antd";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const query = gql`
+	query {
+		meta {
+			cardClasses
+			types
+		}
+	}
+`;
+
+export const BasicFilter = () => (
+	<Query query={query}>
+		{({ data }) => {
+			return (
+				<Select style={{ width: 130 }} defaultValue="Options...">
+					<Select.Option value="Options...">Options...</Select.Option>
+					<Select.OptGroup label="Type">
+						{data.meta.types.map(type => (
+							<Select.Option value={type}>{type}</Select.Option>
+						))}
+					</Select.OptGroup>
+					<Select.OptGroup label="Class">
+						{data.meta.cardClasses.map(cardClass => (
+							<Select.Option value={cardClass}>
+								{cardClass}
+							</Select.Option>
+						))}
+					</Select.OptGroup>
+				</Select>
+			);
+		}}
+	</Query>
+);
